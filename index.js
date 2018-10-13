4854dbe7d31fb549902647488a004157ba1dbdf9
leonardomso <leonardomso11@gmail.com>
2018-10-13 14:22:34 -0300

    Index

---
+++ index.js
@@ -0,0 +1,6 @@
+/* 
+    This repository was created with the intention of helping developers master 
+    their concepts in JavaScript. It is not a requirement, but a guide for 
+    future studies. It is based on an article written by Stephen Curtis and 
+    you can read it here. Feel free to contribute.
+*/
