95232191070ff309590f4067efddc9ddab387212
jmarin217 <25059103+jmarin217@users.noreply.github.com>
2018-10-18 00:14:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
     their concepts in JavaScript. It is not a requirement, but a guide for 
     future studies. It is based on an article written by Stephen Curtis and 
     you can read it here. Feel free to contribute.
-*/
+*/ hhahd
