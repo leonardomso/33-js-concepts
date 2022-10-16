c543d8ebe32a87bafbf7e83ca6b0b12dbe0be8b0
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2022-10-16 15:14:02 -0300

    docs(index.js): modified the copy of the index.js

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 /* 
-    33 JavaScript Concepts is a project created to help JavaScript developers master their skills. It is a compilation of fundamental JavaScript concepts that are important and fundamental. 
+    33 JavaScript Concepts is a project created to help JavaScript developers master their skills. 
+    
+    It is a compilation of fundamental JavaScript concepts that are important and fundamental. 
 
-    This project was inspired by an article written by Stephen Curtis. 
+    JavaScript is a powerful and a very simple language. This language is used to make tasks automation, Web Pages, Servers, Desktop Apps, Databases, etc.
+    
+    Learning the fundamental JS concepts is very important to know how to use this so strong language.
+
+    We are giving you this opportunity for free, so take advantage this manual!
+
+    This project was inspired by an article written by Stephen Curtis.
 
     Any kind of contribution is welcome. Feel free to contribute.
 */
