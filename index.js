a77d841d8de2a777d52478b5dd63df2825e52977
Masterrmind1 <rajputp2007@gmail.com>
2023-10-06 14:25:20 +0530

    aa

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+<<<<<<< HEAD
+=======
+javascript
+var paragraph = document.createElement("p");
+
+paragraph.textContent = "I Just have added Pinch of code here to see Something on Screen. I hope I would help as well. Thanks";
+
+document.body.appendChild(paragraph);
+
+
+>>>>>>> 53378a5 (abcd)
 /* 
     33 JavaScript Concepts is a project created to help JavaScript developers master their skills. It is a compilation of fundamental JavaScript concepts that are important and fundamental. 
 
     This project was inspired by an article written by Stephen Curtis. 
 
     Any kind of contribution is welcome. Feel free to contribute.
+<<<<<<< HEAD
 */
+=======
+*/
+>>>>>>> 53378a5 (abcd)
